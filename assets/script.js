f7c0ddc34ae5c6da7e3d1d2661cac2f0908f9c82
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-06-29 20:47:54 -0500

    next push

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,15 @@ setInterval(function(){
     $("#currentDay").text(moment().format("MMM Do, YYYY [at] hh:mm:ss A"));
 }),
 1000;
+
+$(".saveBtn").on("click", function() {
+    
+    var userTask = $(".present").val();
+
+    localStorage.setItem("userTasks", userTask);
+
+    console.log(userTask);
+    window.location.reload();
+});
+
+$(".present").text(localStorage.getItem("userTasks"));
